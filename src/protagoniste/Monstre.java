08bca21f78be5933a254903c2eaa5483671b9b94
Duarte <101470488+Du4rte66@users.noreply.github.com>
2023-02-14 23:57:37 +0100

    monstre

--- src/protagoniste/Monstre.java
+++ src/protagoniste/Monstre.java
@@ -1,6 +1,8 @@
 package protagoniste;
 
 import java.util.Arrays;
+import java.util.Iterator;
+
 import attaque.Pouvoir;
 
 public class Monstre<P extends Pouvoir> extends EtreVivant{
@@ -30,6 +32,38 @@ public class Monstre<P extends Pouvoir> extends EtreVivant{
 				+ domaine + "]";
 	}
 
-	
+	//classe interne
+	private class GestionAttaque implements Iterator<P> {
+		private P[] attaquesPossibles;
+		private int nbAttaquesPossibles;
+		
+		public GestionAttaque(P[] attaquesPossibles) {
+			this.attaquesPossibles = attaquesPossibles;
+			this.nbAttaquesPossibles = attaquesPossibles.length;
+		}
+		
+		public boolean hasNext() {
+			int i = 0;
+			int nbPouvoirOperationnel = 0;
+			for(P attaque : attaquesPossibles) {
+				if(!attaque.isOperationnel()) {
+					attaquesPossibles[i] = attaquesPossibles[nbAttaquesPossibles - 1];
+					nbAttaquesPossibles--;
+					
+				} else {
+					nbPouvoirOperationnel++;
+				}
+				i++;
+			}
+			return nbPouvoirOperationnel > 0;
+		}
 
+		@Override
+		public P next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+	}
 }
