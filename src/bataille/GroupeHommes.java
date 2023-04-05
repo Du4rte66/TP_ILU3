bc847cb84e268e2c3b9a803c84f02e4679510d7b
Duarte <101470488+Du4rte66@users.noreply.github.com>
2023-04-05 16:58:30 +0200

    supp warning

--- src/bataille/GroupeHommes.java
+++ src/bataille/GroupeHommes.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.NavigableMap;
 import java.util.NavigableSet;
 import java.util.TreeMap;
